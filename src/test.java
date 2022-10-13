45d48fb2548ce87e59c7e071016715861748500c
MSlcim <lciminsu@gmail.com>
2022-10-13 09:50:19 +0900

    2주차 더 맵게

---
+++ src/test.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class test {
+    public static void main(String[] args) {
+    PriorityQueue<Integer> pq = new PriorityQueue<>();
+    pq.add(5);
+    pq.add(2);
+    pq.add(7);
+        System.out.println(pq.peek());
+    }
+}
