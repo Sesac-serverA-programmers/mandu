e1d3f47cbcdabf7ad99517c46d82a292ca039af6
MSlcim <lciminsu@gmail.com>
2022-11-03 13:58:08 +0900

    타겟넘버

--- src/test.java
+++ src/test.java
@@ -1,46 +1,18 @@
-import java.util.*;
+import java.sql.Time;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class test {
-    public String solution(String[] participant, String[] completion) {
-        String answer = "";
-
-        HashMap<String,Integer> map = new HashMap<>();
-        //해쉬맵에 참가자 명단 넣기
-        for(int i = 0 ; i <participant.length;i++){
-            if(map.get(participant[i])==null){
-                map.put(participant[i],1);
-                continue;
-            }
-            map.put(participant[i],map.get(participant[i])+1);
-        }
-
-        //완주한사람은 count-1
-        for(int i = 0 ; i<completion.length;i++){
-            map.put(completion[i],map.get(completion[i])-1);
-        }
-        for (Map.Entry<String, Integer> entry : map.entrySet()) {
-            System.out.println("[key]:" + entry.getKey() + ", [value]:" + entry.getValue());
-        }
+    public static void main(String[] args) {
+        int [][] a= new int[10][15];
+        System.out.println(a.length);
+        System.out.println(a[0].length);
 
 
-
-        for(int i = 0 ; i <participant.length;i++){
-            if(map.get(participant[i])>=1){
-                answer=participant[i];
-                break;
-            }
-        }
-
-
-
-        return answer;
-    }
-    public static void main(String[] args){
-
-        String[] part = {"marina", "josipa", "nikola","vinko","filipa"};
-        String[] comp = {"josipa", "filipa","marina","nikola"};
-
-        test sol = new test();
-        System.out.println(sol.solution(part, comp));
     }
 
 }
