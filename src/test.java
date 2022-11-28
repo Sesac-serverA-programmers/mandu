ceb471d832fe3cf45213bd808486d7961a01aa4e
MSlcim <lciminsu@gmail.com>
2022-11-28 19:41:06 +0900

    f

--- src/test.java
+++ src/test.java
@@ -1,15 +1,12 @@
 import java.io.*;
+import java.util.*;
 import java.util.StringTokenizer;
 
 public class test {
+
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        String tmp = "a b c d e f";
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        while(st.hasMoreTokens()){
-            System.out.println(st.nextToken());
-        }
+
+
     }
 
 }
